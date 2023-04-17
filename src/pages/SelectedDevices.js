228caf8216fa20fc4e81b23c0e15e2f9a92a7ddf
Vyas <reach.vyas.2003@gmail.com>
2023-04-17 13:46:39 -0400

    removed border for question mark

--- src/pages/SelectedDevices.js
+++ src/pages/SelectedDevices.js
@@ -63,12 +63,12 @@ function SelectedDevices({setSelectedDevices, selectedDevices}) {
                                 </th>
                                 <th scope="col" className="px-6 py-3">
                                     <span className="inline-flex items-center">
-                                    Carbon Footprint
-                                    <button type="button" class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-1 py-1 text-center ml-1" onClick={() => {setShowModal(!showModal)}}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
-                                        </svg>
-                                    </button>
+                                        Carbon Footprint
+                                        <button type="button" class="text-gray-900 focus:outline-none font-medium rounded-lg text-sm px-1 py-1 text-center ml-1" onClick={() => {setShowModal(!showModal)}}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
+                                            </svg>
+                                        </button>
                                     </span>
                                 </th>
                             </tr>
